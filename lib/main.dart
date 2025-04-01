997340eb870dbb93e44d40f4d8701f8c6c762c36
Klara Goucovitch <klaragcv@MacBook-Air-de-Klara.local>
2025-04-01 16:03:04 +0200

    changement fond dark par light

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark(),
+      theme: ThemeData.light(),
       home: Scaffold(
         appBar: AppBar(title: const Text('Qualité des Rivières')),
         body: Padding(
